81f9b74eb3d51bb48cf9f6ec3325e55bfbdfc607
Joe Jorden <jljorden@outlook.com>
2024-06-06 10:26:10 -0700

    refactored links

--- app/Links.tsx
+++ app/Links.tsx
@@ -4,30 +4,28 @@ import links from "../data/links.json";
 export default function Links() {
   return (
     <div className="grid grid-cols-2 gap-x-3 gap-y-3">
-      {links.map((link, key) => {
-        return (
-          <div key={key}>
-            <a
-              href={link.href}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="item size-small"
-            >
-              <div className="tile-title">
-                <span className="text">{link.title}</span>
-              </div>
-              <div>
-                <Image
-                  src={`https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${link.imgSrc}&size=32`}
-                  alt="Comics"
-                  width={32}
-                  height={32}
-                />
-              </div>
-            </a>
+      {links.map(({ href, title, imgSrc }, key) => (
+        <a
+          key={key}
+          href={href}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="item size-small"
+        >
+          <div className="tile-title">
+            <span className="text">{title}</span>
           </div>
-        );
-      })}
+          <div>
+            <Image
+              src={`https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${imgSrc}&size=32`}
+              alt="Comics"
+              width={32}
+              height={32}
+              unoptimized
+            />
+          </div>
+        </a>
+      ))}
     </div>
   );
 }
