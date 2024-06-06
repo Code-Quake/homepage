e0c57a3a79fbbdc6302482a3bbd49e6c29433a6d
Joe Jorden <jljorden@outlook.com>
2024-06-06 09:00:47 -0700

    Refactored clock

--- app/Clock.tsx
+++ app/Clock.tsx
@@ -2,43 +2,27 @@
 import React, { useState, useEffect } from "react";
 
 const Clock = () => {
-  const [time, setTime] = useState("");
-  const [date, setDate] = useState("");
+  const [time, setTime] = useState(() => new Date().toLocaleTimeString());
+  const [date, setDate] = useState(() => new Date().toLocaleDateString());
 
-  const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  const timeFormat = navigator.language;
-  const cityName = timeZone.split("/")[1].replaceAll("_", " ");
-  const showSeconds = false;
-  const use12Hour = true;
+  const timeZoneName = Intl.DateTimeFormat()
+    .resolvedOptions()
+    .timeZone.split("/")[1]
+    .replaceAll("_", " ");
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setTime(
-        Intl.DateTimeFormat(timeFormat, {
-          timeZone,
-          hour: "numeric",
-          minute: "numeric",
-          hourCycle: "h12",
-        }).format()
-      );
-      setDate(
-        new Date().toLocaleDateString(timeFormat, {
-          weekday: "long",
-          day: "numeric",
-          year: "numeric",
-          month: "short",
-          timeZone,
-        })
-      );
+      setTime(new Date().toLocaleTimeString());
+      setDate(new Date().toLocaleDateString());
     }, 1000);
 
     return () => clearInterval(interval);
-  }, [timeZone, timeFormat, showSeconds, use12Hour]);
+  }, []);
 
   return (
     <div className="clock">
       <div className="upper">
-        <p className="city">{cityName}</p>
+        <p className="city">{timeZoneName}</p>
         <p className="city">{time}</p>
         <p className="date">{date}</p>
       </div>
