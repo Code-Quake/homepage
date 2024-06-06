2a7d69f30405b93770b71a227e0522b5a90fdf53
Joe Jorden <jljorden@outlook.com>
2024-06-06 10:38:35 -0700

    Refactored systeminfo

--- app/SystemInfo.tsx
+++ app/SystemInfo.tsx
@@ -2,85 +2,45 @@ import React, { createElement } from "react";
 import os from "os";
 
 export const SystemInfoWidget = () => {
-  const meta = {
-    timestamp: new Date(),
-    uptime: os.uptime(),
-    hostname: os.hostname(),
-    username: os.userInfo().username,
-    system: `${os.version()} (${os.arch()} - ${os.release()})`,
-  };
-
-  let ut_sec = os.uptime();
-  let ut_min = ut_sec / 60;
-  let ut_hour = ut_min / 60;
-
-  ut_sec = Math.floor(ut_sec);
-  ut_min = Math.floor(ut_min);
-  ut_hour = Math.floor(ut_hour);
-
-  ut_hour = ut_hour % 60;
-  ut_min = ut_min % 60;
-  ut_sec = ut_sec % 60;
-
-  let cpu_s = os.cpus();
-  let no_of_logical_core = 0;
-  cpu_s.forEach((element) => {
-    no_of_logical_core++;
-  });
+  const { uptime, hostname, version, arch, release } = os;
+  const cpu = os.cpus();
+  const username = os.userInfo().username;
+  const { totalmem, freemem } = os;
+  const networkInterfaces = os.networkInterfaces();
 
   const memory = {
-    total: Math.round(os.totalmem() / (1024 * 1024 * 1024)),
-    free: Math.round(os.freemem() / (1024 * 1024 * 1024)),
-    freePercent: Math.floor((os.freemem() / os.totalmem()) * 100),
+    total: Math.round(totalmem() / (1024 * 1024 * 1024)),
+    free: Math.round(freemem() / (1024 * 1024 * 1024)),
+    freePercent: Math.floor((freemem() / totalmem()) * 100),
   };
 
-  const networkInterfaces = os.networkInterfaces();
-  let ni = [];
-
-  for (let k in networkInterfaces) {
-    let net_info = networkInterfaces[k];
-
+  const networkInfo = Object.entries(networkInterfaces).reduce((acc: any, [k, net_info]) => {
     if (net_info!.length > 2) {
-      let el = createElement(
-        "div",
-        { key: k },
-        createElement("span", { className: "system-header" }, k + ": "),
-        createElement("span", { className: "system" }, net_info![2].address)
-      );
-      ni.push(el);
+      acc.push({ key: k, address: net_info![2].address });
     } else {
-      let el = createElement(
-        "div",
-        { key: k },
-        createElement("span", { className: "system-header" }, k + ": "),
-        createElement("span", { className: "system" }, net_info![1].address)
-      );
-      ni.push(el);
+      acc.push({ key: k, address: net_info![1].address });
     }
-  }
+    return acc;
+  }, []);
+
+  const uptimeInSeconds = uptime();
+  const uptimeInHours = (uptimeInSeconds / 3600).toFixed(2);
 
   return (
     <div className="system-info-wrapper">
       <div className="some-info">
         <p className="host">
-          {meta.hostname} ({meta.username})
+          {hostname()} ({username})
         </p>
         <p className="system">
-          {meta.system}
-          <span className="gap">|</span>
-          Uptime:{" "}
-          {ut_hour +
-            " Hour(s) " +
-            ut_min +
-            " minute(s) and " +
-            ut_sec +
-            " second(s)"}
+          {version()} ({arch()} - {release()})<span className="gap">|</span>
+          Uptime: {uptimeInHours} Hour(s)
         </p>
       </div>
       <div className="some-info">
         <div>
           <span className="system-header">CPU Cores: </span>
-          <span className="system">{no_of_logical_core}</span>
+          <span className="system">{cpu.length}</span>
         </div>
       </div>
       <div className="some-info">
@@ -98,9 +58,14 @@ export const SystemInfoWidget = () => {
         </div>
       </div>
       <div className="some-info">
-        {ni.map((element) => {
-          return element;
-        })}
+        {networkInfo.map(
+          ({ key, address }: { key: string; address: string }) => (
+            <div key={key}>
+              <span className="system-header">{key}: </span>
+              <span className="system">{address}</span>
+            </div>
+          )
+        )}
       </div>
     </div>
   );
