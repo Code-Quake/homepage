8194ab417abeb7283a50564c0e1c21ac54c99cc8
Joe Jorden <jljorden@outlook.com>
2024-06-07 12:11:18 -0700

    Updated CSS

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="box box_purple_first">{children}</div>
+        {children}
       </body>
     </html>
   );
