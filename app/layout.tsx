72518a14fc7c0f09be863bc1de37664b3d54b921
Joe Jorden <jljorden@outlook.com>
2024-06-13 10:48:04 -0700

    Fixed Aurora

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,10 @@
 "use client"; 
 /* eslint-disable @next/next/no-sync-scripts */
-import type { Metadata } from "next";
 import "./globals.css";
 import { Providers } from "./providers";
 import { motion } from "framer-motion";
 import React from "react";
 import { AuroraBackground } from "../modules/AuroraBackground/AuroraBackground";
-
-// This default export is required in a new `pages/_app.js` file.
-// export const metadata: Metadata = {
-//   title: "CodeQuake",
-//   description: "My very own home page",
-// };
 
 export default function RootLayout({
   children,
@@ -31,7 +24,7 @@ export default function RootLayout({
                 duration: 0.8,
                 ease: "easeInOut",
               }}
-              className="relative flex flex-col gap-4 items-center px-4"
+              className="relative flex flex-col gap-4 px-4"
             >
               {children}
             </motion.div>
