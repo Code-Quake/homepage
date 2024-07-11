d0238e976327dc70830ce36d742c430190fa74d0
Joe Jorden <jljorden@outlook.com>
2024-07-11 16:12:39 -0700

    Fixed bugs in dailytext

--- modules/DailyText/DailyText.tsx
+++ modules/DailyText/DailyText.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState, useCallback, useMemo } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import axios from "axios";
 import cheerio from "cheerio";
 import parse, { Element } from "html-react-parser";
@@ -26,53 +26,55 @@ const DailyText: React.FC = () => {
     };
   }, []);
 
-
-  const fetchDailyText = useCallback(async () => {
-    try {
-      const targetUrl = `/text/${today}`;
-      const response = await axios.get(targetUrl);
-      const data = await response.data;
-      const $ = cheerio.load(data);
-      const element = $(`[data-date="${todayTFormat}T00:00:00.000Z"]`);
-
-      const scripturePromises = element
-        .find("a")
-        .map(async (index, element) => {
-          const scriptLink = $(element).attr("href")!.replace("/en/", "");
-          const response = await axios.get("/scripture/" + scriptLink);
-          const test = response.data;
-
-          if (test.items) {
-            return {
-              id: `sc${index}`,
-              citation: test.items[0].title,
-              text: test.items[0].content,
-              reference: !!test.items[0].did,
-            } as IScripture;
-          }
-          return null;
-        })
-        .get();
-
-      const scripts = (await Promise.all(scripturePromises)).filter(Boolean);
-      setScriptureTips(scripts);
-
-      element.find("a").each((index, element) => {
-        $(element).attr("href", `https://wol.jw.org${$(element).attr("href")}`);
-        $(element).attr("id", `sc${index}`);
-        $(element).attr("data", "replace");
-      });
-
-      setDailyText(element.html() ?? null);
-    } catch (error) {
-      console.error("Error fetching daily text:", error);
-      setDailyText(null);
-    }
-  }, [today, todayTFormat]);
-
   useEffect(() => {
+    const fetchDailyText = async () => {
+      try {
+        const targetUrl = `/text/${today}`;
+        const response = await axios.get(targetUrl);
+        const data = response.data;
+        const $ = cheerio.load(data);
+        const element = $(`[data-date="${todayTFormat}T00:00:00.000Z"]`);
+
+        const scripturePromises = element
+          .find("a")
+          .map(async (index, element) => {
+            const scriptLink = $(element).attr("href")!.replace("/en/", "");
+            const response = await axios.get("/scripture/" + scriptLink);
+            const test = response.data;
+
+            if (test.items) {
+              return {
+                id: `sc${index}`,
+                citation: test.items[0].title,
+                text: test.items[0].content,
+                reference: !!test.items[0].did,
+              } as IScripture;
+            }
+            return null;
+          })
+          .get();
+
+        const scripts = (await Promise.all(scripturePromises)).filter(Boolean);
+        setScriptureTips(scripts);
+
+        element.find("a").each((index, element) => {
+          $(element).attr(
+            "href",
+            `https://wol.jw.org${$(element).attr("href")}`
+          );
+          $(element).attr("id", `sc${index}`);
+          $(element).attr("data", "replace");
+        });
+
+        setDailyText(element.html() ?? null);
+      } catch (error) {
+        console.error("Error fetching daily text:", error);
+        setDailyText(null);
+      }
+    };
+
     fetchDailyText();
-  }, [fetchDailyText]);
+  }, [today, todayTFormat]);
 
   if (!dailyText || scriptureTips.length === 0)
     return <Spinner label="Loading" />;
@@ -94,28 +96,41 @@ const DailyText: React.FC = () => {
                 const tip = scriptureTips.find(
                   (item) => item!.id === domNode.attribs.id
                 );
-                const tipText = tip?.text.replace("src=\"", "src=\"https://wol.jw.org");
+                const tipText = tip?.text.replace(
+                  'src="',
+                  'src="https://wol.jw.org'
+                );
                 const parsedTip = parse(tipText ?? "");
-                const width = tip?.reference ? "100rem" : "300px";                
-                return (
-                  <Tooltip
-                    content={parsedTip}
-                    style={{ width: width }}
-                    classNames={{
-                      base: ["before:bg-neutral-400 dark:before:bg-white"],
-                      content: [
-                        "py-2 px-4 shadow-xl",
-                        "text-black bg-gradient-to-br from-slate-600 to-slate-900",
-                      ],
-                    }}
-                  >
+                const width = tip?.reference ? "100rem" : "300px";
+                if (tipText === undefined) {
+                  return (
                     <a href={domNode.attribs.href}>
                       {(domNode.children[0] as any).data
                         ? (domNode.children[0] as any).data
                         : (domNode.children[0] as any).children[0].data}
                     </a>
-                  </Tooltip>
-                );
+                  );
+                } else {
+                  return (
+                    <Tooltip
+                      content={parsedTip}
+                      style={{ width: width }}
+                      classNames={{
+                        base: ["before:bg-neutral-400 dark:before:bg-white"],
+                        content: [
+                          "py-2 px-4 shadow-xl",
+                          "text-black bg-gradient-to-br from-slate-600 to-slate-900",
+                        ],
+                      }}
+                    >
+                      <a href={domNode.attribs.href}>
+                        {(domNode.children[0] as any).data
+                          ? (domNode.children[0] as any).data
+                          : (domNode.children[0] as any).children[0].data}
+                      </a>
+                    </Tooltip>
+                  );
+                }
               }
             },
           })}
