b7aa60551d98650898da455a9d1fe3f07ac832c5
Joe Jorden <jljorden@outlook.com>
2024-08-01 09:59:37 -0700

    Removed dead imports

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,9 +2,6 @@
 "use client";
 
 import { NextUIProvider } from "@nextui-org/react";
-import { motion } from "framer-motion";
-import { AuroraBackground } from "../modules/ui/AuroraBackground";
-import { BackgroundBeams } from "@/modules/ui/BackgroundBeams";
 
 export function Providers({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
