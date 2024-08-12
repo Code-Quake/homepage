924690bdf1d42b2c73faf7df4904acf356de48b8
Joe Jorden <jljorden@outlook.com>
2024-08-12 09:22:49 -0700

    Refactoring

--- modules/Meetings/Meetings.tsx
+++ modules/Meetings/Meetings.tsx
@@ -22,20 +22,12 @@ interface IScripture {
 const getWeek = function () {
   const date = new Date();
   date.setHours(0, 0, 0, 0);
-  // Thursday in current week decides the year.
-  date.setDate(date.getDate() + 3 - ((date.getDay() + 6) % 7));
-  // January 4 is always in week 1.
-  const week1 = new Date(date.getFullYear(), 0, 4);
-  // Adjust to Thursday in week 1 and count number of weeks from date to week1.
-  return (
-    1 +
-    Math.round(
-      ((date.getTime() - week1.getTime()) / 86400000 -
-        3 +
-        ((week1.getDay() + 6) % 7)) /
-        7
-    )
-  );
+  const dayOfWeek = date.getDay();
+  const daysToMonday = dayOfWeek === 0 ? 6 : dayOfWeek - 1;
+  const firstDayOfYear = new Date(date.getFullYear(), 0, 1);
+  const daysSinceYear = Math.round((date.getTime() - firstDayOfYear.getTime()) / 86400000);
+  const weekNum = Math.ceil((daysSinceYear + daysToMonday) / 7);
+  return weekNum;
 };
 
 const Meetings: React.FC = () => {
