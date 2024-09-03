e9bdd2f7724453d86e854119b2c9c83219767e6c
Joe Jorden <jljorden@outlook.com>
2024-09-03 09:37:06 -0700

    Updated build

--- build.ps1
+++ build.ps1
@@ -2,6 +2,6 @@ C:/Users/Jljor/.bun/bin/bun.exe run build
 Copy-Item public -Destination .next\standalone -Recurse
 Copy-Item .next\static -Destination .next\standalone\.next -Recurse
 docker build -t codequake .
-Set-Location -Path p:\docker
+Set-Location -Path p:\docker\codequake
 docker compose up -d
 Set-Location -Path P:\homepage
