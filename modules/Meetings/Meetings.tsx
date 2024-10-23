2bc2e17e24b7410b8147069d0c49f668f7b29752
Joe Jorden <5692870+Code-Quake@users.noreply.github.com>
2024-10-23 10:41:58 -0700

    Added watchtower to meetings

--- modules/Meetings/Meetings.tsx
+++ modules/Meetings/Meetings.tsx
@@ -20,20 +20,31 @@ interface IScripture {
   reference: boolean;
 }
 
+interface IWatchtower {
+  content: string;
+  imageUrl: string;
+  publicationTitle: string;
+  title: string;
+}
+
 const getWeek = function () {
   const date = new Date();
   date.setHours(0, 0, 0, 0);
   const dayOfWeek = date.getDay();
   const daysToMonday = dayOfWeek === 0 ? 6 : dayOfWeek - 1;
   const firstDayOfYear = new Date(date.getFullYear(), 0, 1);
-  const daysSinceYear = Math.round((date.getTime() - firstDayOfYear.getTime()) / 86400000);
+  const daysSinceYear = Math.round(
+    (date.getTime() - firstDayOfYear.getTime()) / 86400000
+  );
   const weekNum = Math.ceil((daysSinceYear + daysToMonday) / 7);
   return weekNum;
 };
 
 const Meetings: React.FC = () => {
   const [meetings, setMeetings] = useState<string | null>(null);
+  const [watchtower, setWatchtower] = useState<string | null>(null);
   const [scriptureTips, setScriptureTips] = useState<(IScripture | null)[]>([]);
+  const [wtScriptureTips, setWTScriptureTips] = useState<(IScripture | null)[]>([]);
   const [weekNum, setWeekNum] = useState<number>(0);
 
   const { year } = useMemo(() => {
@@ -51,7 +62,7 @@ const Meetings: React.FC = () => {
   }, []);
 
   useEffect(() => {
-    const fetchDailyText = async () => {
+    const fetchMidweek = async () => {
       try {
         const targetUrl = `/meetings/${year}/${weekNum}`;
         const response = await axios.get(targetUrl);
@@ -63,6 +74,29 @@ const Meetings: React.FC = () => {
           .find("#content")
           .find("#article")
           .find(".todayItems");
+
+        const wtLinkSource = $("*")
+          .find("#wrapper")
+          .find("#regionMain")
+          .find("#content")
+          .find("#article")
+          .find(".todayItems")
+          .find(".pub-w")
+          .find(".itemData")
+          .find("p")
+          .find("a");
+
+        const wtLink = $(wtLinkSource).attr("href")!.replace("/en/", "");
+
+        const wtResponse = await axios.get("/scripture/" + wtLink, {
+          maxRedirects: 0,
+          beforeRedirect: () => {
+            return "";
+          },
+        });
+
+        const content = `<div>${wtResponse.data.items[0].content}</div>`;
+        const wtContent = $(content);
 
         const scripturePromises = meetings
           .find("a")
@@ -77,21 +111,24 @@ const Meetings: React.FC = () => {
               return null;
             }
 
-            try{
-            const response = await axios.get("/scripture/" + scriptLink,{ maxRedirects: 0, beforeRedirect: ()=>{ return ""}});
-            const test = response.data;
+            try {
+              const response = await axios.get("/scripture/" + scriptLink, {
+                maxRedirects: 0,
+                beforeRedirect: () => {
+                  return "";
+                },
+              });
+              const test = response.data;
 
-            if (test.items) {
-              return {
-                id: `sc${index}`,
-                citation: test.items[0].title,
-                text: test.items[0].content,
-                reference: !!test.items[0].did,
-              } as IScripture;
-            }
-          }
-            catch(error: any)
-            {
+              if (test.items) {
+                return {
+                  id: `sc${index}`,
+                  citation: test.items[0].title,
+                  text: test.items[0].content,
+                  reference: !!test.items[0].did,
+                } as IScripture;
+              }
+            } catch (error: any) {
               return null;
             }
             return null;
@@ -118,61 +155,149 @@ const Meetings: React.FC = () => {
           $(element).remove();
         });
 
+
+                const wtScripturePromises = wtContent
+                  .find("a")
+                  .map(async (index, element) => {
+                    const scriptLink = $(element)
+                      .attr("href")!
+                      .replace("/en/", "");
+
+                    if (
+                      scriptLink.indexOf("finder") > 0 ||
+                      scriptLink.indexOf("data-video") > 0 ||
+                      $(element).attr("class")?.includes("noTooltips")
+                    ) {
+                      return null;
+                    }
+
+                    try {
+                      const response = await axios.get(
+                        "/scripture/" + scriptLink,
+                        {
+                          maxRedirects: 0,
+                          beforeRedirect: () => {
+                            return "";
+                          },
+                        }
+                      );
+                      const test = response.data;
+
+                      if (test.items) {
+                        return {
+                          id: `sc${index}`,
+                          citation: test.items[0].title,
+                          text: test.items[0].content,
+                          reference: !!test.items[0].did,
+                        } as IScripture;
+                      }
+                    } catch (error: any) {
+                      return null;
+                    }
+                    return null;
+                  })
+                  .get();
+
+        const wtScripts = (await Promise.all(wtScripturePromises)).filter(Boolean);
+        setWTScriptureTips(wtScripts);
+
+        wtContent
+          .find("a")
+          .each((index, element) => {
+            $(element).attr(
+              "href",
+              `https://wol.jw.org${$(element).attr("href")}`
+            );
+            $(element).attr("id", `sc${index}`);
+            $(element).attr("data", "replace");
+          });
+
+        wtContent
+          .find("img")
+          .each((index, element) => {
+            $(element).attr(
+              "src",
+              `https://wol.jw.org${$(element).attr("src")}`
+            );
+          });
+
+        wtContent
+          .find("textArea")
+          .each((index, element) => {
+            $(element).remove();
+          });
+
         setMeetings($(meetings).html());
+        setWatchtower($(wtContent).html());
+
       } catch (error) {
         console.error("Error fetching daily text:", error);
       }
     };
 
-    fetchDailyText();
+    fetchMidweek();
   }, [year, weekNum]);
 
   if (!meetings) return <Spinner label="Loading" />;
 
   return (
-    <div className="w-[calc(100%)] mx-auto relative">
-      <div className="flex justify-center w-full items-center mt-2 mb-2">
-        <span
-          className="tooltip"
-          data-tooltip="Previous Week"
-          data-tooltip-position="left"
-        >
-          <FontAwesomeIcon
-            onClick={() => setWeekNum(weekNum - 1)}
-            icon={faAnglesLeft}
-            role="button"
-            aria-label="Previous Week"
-            className="pl-2.5 text-xl text-[var(--primary)]"
-          />
-        </span>
-        <span
-          className="tooltip"
-          data-tooltip="This Week"
-          data-tooltip-position="bottom"
-        >
-          <FontAwesomeIcon
-            onClick={() => setWeekNum(getWeek())}
-            icon={faCalendarWeek}
-            role="button"
-            aria-label="This Week"
-            className="pl-2.5 text-xl text-[var(--primary-fuchsia)]"
-          />
-        </span>
-        <span
-          className="tooltip"
-          data-tooltip="Next Week"
-          data-tooltip-position="right"
-        >
-          <FontAwesomeIcon
-            onClick={() => setWeekNum(weekNum + 1)}
-            icon={faAnglesRight}
-            role="button"
-            aria-label="Next Week"
-            className="pl-2.5 text-xl text-[var(--primary)]"
-          />
-        </span>
+    <div className="checkbox-wrapper-8 w-[calc(100%)] mx-auto relative">
+      <input
+        className="tgl tgl-skewed"
+        id="cbMeeting"
+        type="checkbox"
+        defaultChecked
+      />
+      <label
+        className="tgl-btn mt-2"
+        data-tg-off="WT"
+        data-tg-on="Mid"
+        htmlFor="cbMeeting"
+      ></label>
+      <div className="flex justify-center w-full items-center relative">
+        <div className="w-28 top-[-25px] relative">
+          <span
+            className="tooltip"
+            data-tooltip="Previous Week"
+            data-tooltip-position="left"
+          >
+            <FontAwesomeIcon
+              onClick={() => setWeekNum(weekNum - 1)}
+              icon={faAnglesLeft}
+              role="button"
+              aria-label="Previous Week"
+              className="pl-2.5 text-xl text-[var(--primary)]"
+            />
+          </span>
+          <span
+            className="tooltip"
+            data-tooltip="This Week"
+            data-tooltip-position="bottom"
+          >
+            <FontAwesomeIcon
+              onClick={() => setWeekNum(getWeek())}
+              icon={faCalendarWeek}
+              role="button"
+              aria-label="This Week"
+              className="pl-2.5 text-xl text-[var(--primary-fuchsia)]"
+            />
+          </span>
+          <span
+            className="tooltip"
+            data-tooltip="Next Week"
+            data-tooltip-position="right"
+          >
+            <FontAwesomeIcon
+              onClick={() => setWeekNum(weekNum + 1)}
+              icon={faAnglesRight}
+              role="button"
+              aria-label="Next Week"
+              className="pl-2.5 text-xl text-[var(--primary)]"
+            />
+          </span>
+        </div>
       </div>
-      <div className="relative h-[500px] pl-2 pr-2 overflow-auto [scrollbar-width:none] [-ms-overflow-style:none] [-webkit-overflow-scrolling:touch]">
+      <div className="relative h-[500px] pl-2 pr-2 overflow-auto [scrollbar-width:none] [-ms-overflow-style:none] [-webkit-overflow-scrolling:touch] contentMidweek">
         {parse(meetings, {
           replace(domNode) {
             if (
@@ -181,6 +306,55 @@ const Meetings: React.FC = () => {
               domNode.attribs.data === "replace"
             ) {
               const tip = scriptureTips.find(
+                (item) => item!.id === domNode.attribs.id
+              );
+              const tipText = tip?.text
+                .replaceAll('src="', 'src="https://wol.jw.org')
+                .replaceAll('href="', 'href="https://wol.jw.org');
+              const parsedTip = parse(tipText ?? "");
+              if (tipText === undefined) {
+                return (
+                  <a href={domNode.attribs.href}>
+                    {(domNode.children[0] as any).data
+                      ? (domNode.children[0] as any).data
+                      : (domNode.children[0] as any).children[0].data}
+                  </a>
+                );
+              } else {
+                return (
+                  <Tooltip
+                    content={parsedTip}
+                    classNames={{
+                      base: [
+                        "before:bg-neutral-400 dark:before:bg-white h-fit max-h-60 overflow-auto [scrollbar-width:none] [-ms-overflow-style:none] [-webkit-overflow-scrolling:touch]",
+                      ],
+                      content: [
+                        "py-2 px-4 shadow-xl",
+                        "text-black bg-gradient-to-br from-[var(--accent-blue)] to-[var(--dark-blue)] w-fit w-min-20 max-w-96 overflow-auto",
+                      ],
+                    }}
+                  >
+                    <a href={domNode.attribs.href}>
+                      {(domNode.children[0] as any).data
+                        ? (domNode.children[0] as any).data
+                        : (domNode.children[0] as any).children[0].data}
+                    </a>
+                  </Tooltip>
+                );
+              }
+            }
+          },
+        })}
+      </div>
+      <div className="relative h-[500px] pl-2 pr-2 overflow-auto [scrollbar-width:none] [-ms-overflow-style:none] [-webkit-overflow-scrolling:touch] contentWatchtower">
+        {parse(watchtower!, {
+          replace(domNode) {
+            if (
+              domNode instanceof Element &&
+              domNode.attribs &&
+              domNode.attribs.data === "replace"
+            ) {
+              const tip = wtScriptureTips.find(
                 (item) => item!.id === domNode.attribs.id
               );
               const tipText = tip?.text
